a4b83b083e30e8c514eea261904634872c88100e
Jay Mehta <jaysmehta@gmail.com>
2024-10-01 22:37:45 -0400

    all edge cases handled

--- client/src/components/ProfessorDetails.js
+++ client/src/components/ProfessorDetails.js
@@ -6,9 +6,9 @@ import config from '../config';
 import Grid from '@mui/material/Grid';
 import { Typography, Card, CardContent, Button, Chip, Rating, Box } from '@mui/material';
 import { Pie } from 'react-chartjs-2';
-import { Chart, ArcElement, Tooltip, Legend } from 'chart.js'; // Import necessary components from chart.js
+import { Chart, ArcElement, Tooltip, Legend } from 'chart.js'; 
 import StarIcon from '@mui/icons-material/Star';
-// Register the components
+
 Chart.register(ArcElement, Tooltip, Legend);
 function ProfessorDetails() {
   const { alias } = useParams();
@@ -38,33 +38,33 @@ function ProfessorDetails() {
   if (!professor) {
     return <div>No professor found for this alias</div>;
   }
-  // Prepare chart data with colors more suited to the website
+
   const pieData = {
     labels: ['Positive', 'Negative'],
     datasets: [
       {
         label: 'Review Distribution',
         data: [professor.positive_percentage, professor.negative_percentage],
-        backgroundColor: ['#66bb6a', '#ef5350'], // Updated colors
+        backgroundColor: ['#66bb6a', '#ef5350'], 
         hoverOffset: 4,
       },
     ],
   };
-  // Pie chart options with legend below the chart
+
   const pieOptions = {
     plugins: {
       legend: {
-        position: 'bottom', // Move labels to below the chart
+        position: 'bottom', 
       },
     },
   };
-  // Extract the top 3 tags by number of votes
+
   const sortedTags = Object.entries(professor.tags || {}).sort((a, b) => b[1] - a[1]);
   const topTags = sortedTags.slice(0, 3);
   const otherTags = sortedTags.slice(3);
-  // Known attributes to be displayed
+
   const knownAttributes = ['NAME', 'ALIAS', 'EMAIL', 'total_reviews', 'rating', 'tags', 'href', 'positive_percentage', 'negative_percentage'];
-  // Split dynamic attributes into two groups for two-column display
+
   const dynamicAttributes = Object.keys(professor).filter(
     (key) => !knownAttributes.includes(key) && typeof professor[key] === 'string' && key !== '_id' && professor[key] !== ""
   );
@@ -74,23 +74,18 @@ function ProfessorDetails() {
   return (
     <Card sx={{ padding: 2 }}>
       <CardContent>
-        {/* Professor Name */}
         <Typography variant="h3" align="center" gutterBottom sx={{ fontWeight: 'bold', paddingBottom: 2 }}>
           {professor.NAME}
         </Typography>
         <Grid container spacing={4} paddingBottom={2}>
-          {/* Left Section: Professor Attributes with background color */}
           <Grid item xs={12} md={8}>
             <Box sx={{ backgroundColor: '#2E3B55', padding: 3, borderRadius: 2 }}>
-              {/* Two columns of attributes */}
               <Grid container spacing={4} alignItems="start">
                 <Grid item xs={6}>
-                  {/* Known Attributes */}
                   <Box>
                     <Typography variant="body1" gutterBottom sx={{ color: '#ffffff' }}><strong>Alias</strong>: {professor.ALIAS}</Typography>
                     <Typography variant="body1" gutterBottom sx={{ color: '#ffffff' }}><strong>Email</strong>: {professor.EMAIL}</Typography>
                   </Box>
-                  {/* First Half of Dynamic Attributes */}
                   {firstHalfAttributes.map((key) => (
                     <Typography key={key} variant="body1" gutterBottom sx={{ color: '#ffffff' }}>
                       <strong>{key.replace(/_/g, ' ')}</strong>: {professor[key]}
@@ -98,7 +93,6 @@ function ProfessorDetails() {
                   ))}
                 </Grid>
                 <Grid item xs={6}>
-                  {/* Second Half of Dynamic Attributes */}
                   {secondHalfAttributes.map((key) => (
                     <Typography key={key} variant="body1" gutterBottom sx={{ color: '#ffffff' }}>
                       <strong>{key.replace(/_/g, ' ')}</strong>: {professor[key]}
@@ -108,8 +102,10 @@ function ProfessorDetails() {
               </Grid>
             </Box>
           </Grid>
-          {/* Right Section: Rating and RateMyProfessors Button */}
+          
+
           <Grid item xs={12} md={4} sx={{ textAlign: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          {professor.rating !== "Professor not found" ? 
             <Box>
               <Typography variant="h4" gutterBottom sx={{ fontWeight: 'bold' }}>
                 Rating: {professor.rating}
@@ -122,7 +118,6 @@ function ProfessorDetails() {
                 size="large"
                 icon={<StarIcon fontSize="inherit" />}
               />
-              {/* RateMyProfessors Link - Now placed directly below the rating */}
               <Button
                 variant="contained"
                 color="primary"
@@ -133,15 +128,19 @@ function ProfessorDetails() {
                 Rate My Professors Profile
               </Button>
             </Box>
-          </Grid>
+            :
+            <Typography variant="body2">No Rate My Professor Page exists: Professor is new or profile exists under a different name</Typography> }
+          </Grid> 
         </Grid>
+        
+        { professor.rating !== "Professor not found" && 
+        (professor.rating !== 'N/A' ?
         <Grid container spacing={4}>
-          {/* Left Section: Pie Chart */}
           <Grid item xs={12} md={4}>
             <Typography variant="h5" gutterBottom sx={{ fontWeight: 'bold' }}>
               Review Distribution
             </Typography>
-            <div style={{ width: '250px', height: '250px', paddingLeft:60 }}>  {/* Reduce width and height */}
+            <div style={{ width: '250px', height: '250px', paddingLeft:60 }}>  
               <Pie 
                 data={pieData} 
                 options={pieOptions} 
@@ -152,7 +151,6 @@ function ProfessorDetails() {
              </Typography>
             
           </Grid>
-          {/* Middle Section: Top 3 Tags - Adjusted padding to move it to the left */}
           <Grid item xs={12} md={4} sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', flexDirection: 'column', marginLeft: '-20px' }}>
             <Typography variant="h5" gutterBottom sx={{ fontWeight: 'bold' }}>
               Top 3 Tags
@@ -165,7 +163,6 @@ function ProfessorDetails() {
               ))}
             </Box>
           </Grid>
-          {/* Right Section: Other Tags with customized chips */}
           <Grid item xs={12} md={4}>
             <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold' }}>
               Other Tags
@@ -178,8 +175,13 @@ function ProfessorDetails() {
               <Typography variant="body2">No other tags available</Typography>
             )}
           </Grid>
-        </Grid>
+        </Grid> :
+        <Typography variant="body2">Not enough data for review distributions and tags</Typography> )
+        } 
+      
+        
       </CardContent>
+      
     </Card>
   );
 }
